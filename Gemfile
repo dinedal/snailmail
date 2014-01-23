065191961980eb733661de6c938af03a36ca8e5b
Paul Bergeron <paul.d.bergeron@gmail.com>
2014-01-22 17:58:45 -0800

    Ohm to bleeding edge, connect to redis

--- Gemfile
+++ Gemfile
@@ -9,4 +9,5 @@ gem 'pry-doc', '~> 0.5.1', :groups => [:development]
 gem 'twilio-ruby', '~> 3.11.4'
 gem 'lob', '~> 1.2'
 gem 'dotenv', '~> 0.9.0'
-gem 'ohm', '~> 1.3.2'
+gem 'redic', '~> 0.0.8'
+gem 'ohm', '2.0.0.rc1'
--- lib/snailmail.rb
+++ lib/snailmail.rb
@@ -6,10 +6,15 @@ module Snailmail
   TWILLIO_SID       = ENV['TWILLIO_SID']
   TWILLIO_AUTHTOKEN = ENV['TWILLIO_AUTHTOKEN']
   ADMIN_PASSWORD    = ENV['ADMIN_PASSWORD']
+  REDIS_URL         = ENV['REDISTOGO_URL']
 end
 
 require 'sinatra/base'
 require 'lob'
 require 'twilio-ruby'
+require 'redic'
+require 'ohm'
+
+Ohm.redis = Redic.new(Snailmail::REDIS_URL)
 
 Dir["./lib/snailmail/**"].map(&method(:require))
