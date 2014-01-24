c9955f50846cf732efb58316e4c333f94e3c7af6
Paul Bergeron <paul.d.bergeron@gmail.com>
2014-01-23 18:29:01 -0800

    Silence stderr on console streaming to clear up logs

--- lib/snailmail/transcription.rb
+++ lib/snailmail/transcription.rb
@@ -9,7 +9,7 @@ class Snailmail::Transcription
     # actually works =D
     #
 
-    json_result = JSON.parse %x{curl #{url} 2> /dev/null | ffmpeg -i pipe:0 -vn -sn -acodec flac -f flac pipe:1 | curl -v -X POST -H 'Content-Type: audio/x-flac; rate=8000' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7' --data-binary @- "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US&results=10"}
+    json_result = JSON.parse %x{curl #{url} 2> /dev/null | ffmpeg -i pipe:0 -vn -sn -acodec flac -f flac pipe:1 2> /dev/null | curl -v -X POST -H 'Content-Type: audio/x-flac; rate=8000' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7' --data-binary @- "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US&results=10" 2> /dev/null}
 
     json_result["hypotheses"].sort_by{|h| -h["confidence"]}.first["utterance"]
   end
