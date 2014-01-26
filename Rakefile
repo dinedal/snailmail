c1cca6b4414ca97f363b1cb15e46e775f9b43949
Paul Bergeron <paul.d.bergeron@gmail.com>
2014-01-26 14:08:49 -0800

    Add automated test that verifies the transcription process works correctly

--- Rakefile
+++ Rakefile
@@ -2,3 +2,22 @@ require 'rake/testtask'
 Rake::TestTask.new do |t|
   t.pattern = "spec/*_spec.rb"
 end
+
+desc "Tests Twillio -> FFMpeg -> Google transcription in practice"
+task :transcription_test do
+  require "./lib/snailmail.rb"
+  test_recording_url = %q{http://api.twilio.com/2010-04-01/Accounts/ACb84633f66d5af69d5d09b9b6535f1ed7/Recordings/RE6ff45ca6335a9b32a060fc997987f93e}
+
+  expected_output = "hello this is paul trying out the recording service"
+
+  transcription = Snailmail::Transcription.wav_to_text(test_recording_url).downcase
+
+  if expected_output != transcription
+    $stderr.puts "Transcription failed"
+    $stderr.puts "Expected: #{expected_output}"
+    $stderr.puts "Transcription returned: #{transcription}"
+    raise Exception, "Transcription failed"
+  else
+    puts "Transcription test passed"
+  end
+end
--- lib/snailmail/transcription.rb
+++ lib/snailmail/transcription.rb
@@ -4,10 +4,8 @@ class Snailmail::Transcription
   def self.wav_to_text(url)
     # Sorry twillio, your service costs money and google does a good job
     #
-    # Here I stream the recording url through ffmpeg to get flacc, and
-    # simultaneously upload to google for analysis, all while praying it
-    # actually works =D
-    #
+    # Here I stream the recording url through ffmpeg to get flac, and
+    # simultaneously upload to google for transcription
 
     json_result = JSON.parse %x{curl #{url} 2> /dev/null | ffmpeg -i pipe:0 -vn -sn -acodec flac -f flac pipe:1 2> /dev/null | curl -v -X POST -H 'Content-Type: audio/x-flac; rate=8000' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7' --data-binary @- "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US&results=10" 2> /dev/null}
 
